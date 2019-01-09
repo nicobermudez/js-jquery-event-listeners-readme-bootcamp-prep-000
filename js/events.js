608f56793ce9962c44e8304da33ed7762437b46d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-09 18:00:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey");
+    return;
+  })
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    return; 
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function (key) {
+    if(key.which == 71) {
+      alert('g was pressed');
+      return;
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() { 
+  alert('Your form is going to be submitted now.');
+  return;
+  })
+}
+
+$(document).ready(function(){
 
 });
